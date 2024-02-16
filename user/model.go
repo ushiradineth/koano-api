08d6e7354310c0557098b9dffb515cfc2dbc187a
Ushira Dineth <ushiradineth@gmail.com>
2024-02-16 21:43:26 +0530

    fix: updated model order

--- user/model.go
+++ user/model.go
@@ -8,8 +8,8 @@ import (
 
 type User struct {
 	ID        uuid.UUID `db:"id"`
+	CreatedAt time.Time `db:"created_at"`
 	Name      string    `db:"name"`
 	Email     string    `db:"email"`
 	Password  string    `db:"password"`
-	CreatedAt time.Time `db:"created_at"`
 }
