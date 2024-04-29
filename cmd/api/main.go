f8613e26bc954e06ce44e95d19a58469f1120bca
Ushira Dineth <ushiradineth@gmail.com>
2024-04-29 23:53:48 +0400

    perf(main): graceful shutdowns

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,36 +4,71 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
-	"github.com/ushiradineth/cron-be/database"
 	"github.com/ushiradineth/cron-be/api/event"
 	"github.com/ushiradineth/cron-be/api/user"
+	"github.com/ushiradineth/cron-be/database"
 )
+
+// Graceful shutdowns based on https://grafana.com/blog/2024/02/09/how-i-write-http-services-in-go-after-13-years/
 
 func main() {
 	ctx := context.Background()
-	if err := run(ctx, os.Stdout); err != nil {
+	if err := run(ctx, os.Stdout, os.Args); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context, w io.Writer) error {
+func run(ctx context.Context, w io.Writer, args []string) error {
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
 
 	godotenv.Load(".env")
 	db := database.Configure()
 
-	return routes(db)
+	routes := routes(db)
+
+	httpServer := &http.Server{
+		Addr:    fmt.Sprintf(":%s", os.Getenv("PORT")),
+		Handler: routes,
+	}
+
+	go func() {
+		log.Printf("Listening on %s\n", httpServer.Addr)
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Fprintf(os.Stderr, "Error listening and serving: %s\n", err)
+		}
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+		shutdownCtx := context.Background()
+		shutdownCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
+		if err := httpServer.Shutdown(shutdownCtx); err != nil {
+			fmt.Fprintf(os.Stderr, "error shutting down http server: %s\n", err)
+		}
+	}()
+
+	wg.Wait()
+
+	return nil
 }
 
-func routes(db *sqlx.DB) error {
+func routes(db *sqlx.DB) http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /user", func(w http.ResponseWriter, r *http.Request) { user.GetUserHandler(w, r, db) })
@@ -50,7 +85,5 @@ func routes(db *sqlx.DB) error {
 	mux.HandleFunc("DELETE /event/{event_id}", func(w http.ResponseWriter, r *http.Request) { event.DeleteEventHandler(w, r, db) })
 	mux.HandleFunc("GET /event/user", func(w http.ResponseWriter, r *http.Request) { event.GetUserEventsHandler(w, r, db) })
 
-	err := http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), mux)
-
-	return err
+	return mux
 }
