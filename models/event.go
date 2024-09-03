daded125b500f7292c2c1bf7e4259771f48749db
Ushira Dineth <ushiradineth@gmail.com>
2024-09-04 00:43:57 +0400

    feat(models): add json annotations

--- models/event.go
+++ models/event.go
@@ -8,11 +8,11 @@ import (
 
 type Event struct {
 	ID        uuid.UUID `db:"id"`
-	UserID    uuid.UUID `db:"user_id"`
-	CreatedAt time.Time `db:"created_at"`
-	Title     string    `db:"title"`
-	Start     time.Time `db:"start_time"`
-	End       time.Time `db:"end_time"`
-	Timezone  string    `db:"timezone"`
-	Repeated  string    `db:"repeated"`
+	UserID    uuid.UUID `db:"user_id" json:"user_id"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	Title     string    `db:"title" json:"title"`
+	Start     time.Time `db:"start_time" json:"start_time"`
+	End       time.Time `db:"end_time" json:"end_time"`
+	Timezone  string    `db:"timezone" json:"timezone"`
+	Repeated  string    `db:"repeated" json:"repeated"`
 }
--- models/user.go
+++ models/user.go
@@ -7,9 +7,9 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `db:"id"`
-	CreatedAt time.Time `db:"created_at"`
-	Name      string    `db:"name"`
-	Email     string    `db:"email"`
-	Password  string    `db:"password"`
+	ID        uuid.UUID `db:"id" json:"id"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	Name      string    `db:"name" json:"name"`
+	Email     string    `db:"email" json:"email"`
+	Password  string    `db:"password" json:"password"`
 }
