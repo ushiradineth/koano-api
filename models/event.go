7b60ff4a2748fdd4c74aae2793522e4b8c9005af
Ushira Dineth <ushiradineth@gmail.com>
2025-01-04 23:24:20 +0400

    feat: add extra context to models

--- models/event.go
+++ models/event.go
@@ -7,12 +7,16 @@ import (
 )
 
 type Event struct {
-	ID        uuid.UUID `db:"id" json:"id"`
-	UserID    uuid.UUID `db:"user_id" json:"user_id"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	Title     string    `db:"title" json:"title"`
-	Start     time.Time `db:"start_time" json:"start_time"`
-	End       time.Time `db:"end_time" json:"end_time"`
-	Timezone  string    `db:"timezone" json:"timezone"`
-	Repeated  string    `db:"repeated" json:"repeated"`
+	ID        uuid.UUID  `db:"id" json:"id"`
+	CreatedAt time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time  `db:"updated_at" json:"updated_at"`
+	DeletedAt *time.Time `db:"deleted_at" json:"deleted_at"`
+	Active    bool       `db:"active" json:"active"`
+
+	UserID   uuid.UUID `db:"user_id" json:"user_id"`
+	Title    string    `db:"title" json:"title"`
+	Start    time.Time `db:"start_time" json:"start_time"`
+	End      time.Time `db:"end_time" json:"end_time"`
+	Timezone string    `db:"timezone" json:"timezone"`
+	Repeated string    `db:"repeated" json:"repeated"`
 }
--- models/user.go
+++ models/user.go
@@ -7,9 +7,13 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `db:"id" json:"id"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	Name      string    `db:"name" json:"name"`
-	Email     string    `db:"email" json:"email"`
-	Password  string    `db:"password" json:"password"`
+	ID        uuid.UUID  `db:"id" json:"id"`
+	CreatedAt time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time  `db:"updated_at" json:"updated_at"`
+	DeletedAt *time.Time `db:"deleted_at" json:"deleted_at"`
+	Active    bool       `db:"active" json:"active"`
+
+	Name     string `db:"name" json:"name"`
+	Email    string `db:"email" json:"email"`
+	Password string `db:"password" json:"password"`
 }
